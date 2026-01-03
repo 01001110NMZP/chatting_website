af29be27e26e3116b9868efc9ee7cd59ad38de98
Nima Masoumi <159164960+01001110NMZP@users.noreply.github.com>
2026-01-03 03:34:23 +0330

    Delete chatting_website directory
    made a mistake while uploading, fixing it now

--- chatting_website/chat_rooms/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Message
-
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ["message_text"]
-        labels = {"message_text": ""}
--- chatting_website/chat_rooms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Home, ChatDisplayCreateView
-
-app_name = "chat_rooms"
-
-urlpatterns = [
-    path("", Home.as_view(), name="home"),
-    path("chatroom/", ChatDisplayCreateView.as_view(), name="selected_chatroom"),
-]
